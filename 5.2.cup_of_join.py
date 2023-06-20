921303055f09a4ff6a646974513189537a239973
yaniv051293 <yaniv051293@gmail.com>
2023-06-20 14:10:02 +0300

    change sep

--- 5.2.cup_of_join.py
+++ 5.2.cup_of_join.py
@@ -1,15 +1,15 @@
-def join(*lists, sep='-'):
+def join(*lists, separate='-'):
     """Concatenates an arbitrary number of lists."""
     if lists is None:
         return None
     result = []
     for lst in lists:
-        result = result + lst + [sep]
+        result = result + lst + [separate]
     return result[:-1]
 
 
 def main():
-    assert (join([1, 2], [8], [9, 5, 6], sep='@') == [1, 2, '@', 8, '@', 9, 5, 6])
+    assert (join([1, 2], [8], [9, 5, 6], separate='@') == [1, 2, '@', 8, '@', 9, 5, 6])
     print("All work good")
 
 
